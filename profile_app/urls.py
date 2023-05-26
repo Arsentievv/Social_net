977b291d516c0c3e00afd5d296be328ec48dd97f
Aleksey Arsentyev <arsentief@gmail.com>
2023-05-26 12:39:30 +0300

    adding//removing friends added

--- profile_app/urls.py
+++ profile_app/urls.py
@@ -3,7 +3,10 @@ from .views import (my_profile_view,
                     invites_received_view,
                     profiles_list_view,
                     invite_profile_list_view,
-                    ProfileListView)
+                    ProfileListView,
+                    send_invatation,
+                    remove_from_friends,
+                    )
 
 app_name = 'profiles'
 
@@ -12,4 +15,6 @@ urlpatterns = [
     path('my_invites/', invites_received_view, name='my_invites'),
     path('profiles_list/', ProfileListView.as_view(), name='profile_list'),
     path('invite_profiles/', invite_profile_list_view, name='invite_profiles'),
+    path('send_invite/', send_invatation, name='send_invite'),
+    path('remove_friend/', remove_from_friends, name='remove_friend'),
 ]
--- profile_app/views.py
+++ profile_app/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from profile_app.models import Profile, Relationship
 from .forms import ProfileModelForm
+from django.db.models import Q
 
 
 
@@ -87,5 +88,29 @@ class ProfileListView(generic.ListView):
         if len(self.get_queryset()) == 0:
             context['is_empty'] = True
         return context
+
+def send_invatation(request):
+    if request.method == 'POST':
+        pk = request.POST.get('profile_pk')
+        user = request.user
+        sender = Profile.objects.get(user=user)
+        receiver = Profile.objects.get(pk=pk)
+        rel = Relationship.objects.create(sender=sender, receiver=receiver, status='send')
+        return redirect(request.META.get('HTTP_REFERER'))
+    return redirect('profiles:my_profile')
+
+def remove_from_friends(request):
+    if request.method == 'POST':
+        pk = request.POST.get('profile_pk')
+        user = request.user
+        sender = Profile.objects.get(user=user)
+        receiver = Profile.objects.get(pk=pk)
+
+        rel = Relationship.objects.get(
+            (Q(sender=sender) & Q(receiver=receiver) | (Q(sender=receiver) & Q(receiver=sender)))
+        )
+        rel.delete()
+        return redirect(request.META.get('HTTP_REFERER'))
+    return redirect('profiles:my_profile')
 
 
